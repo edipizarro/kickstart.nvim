456bfab64f151faf4d6bb1ae68b3269522b3ccbc
Edison Pizarro <edison.pizarro.godoy@gmail.com>
2024-09-30 16:51:17 -0300

    use gopls and ruff lsp

--- init.lua
+++ init.lua
@@ -606,8 +606,9 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         pyright = {},
+        ruff = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
