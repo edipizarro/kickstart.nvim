f33463e457df1454a901528677ce2b10649c23f5
Edison Pizarro Godoy <edison.pizarro@front.com>
2024-11-20 00:54:40 -0300

    add lsps

--- init.lua
+++ init.lua
@@ -609,6 +609,9 @@ require('lazy').setup({
         gopls = {},
         pyright = {},
         ruff = {},
+        ts_ls = {},
+        eslint = {},
+        terraformls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
