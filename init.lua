4b0b5300fce69c791296f0409b16a13e1a327255
Edison Pizarro Godoy <edison.pizarro@front.com>
2024-11-21 17:48:05 -0300

    add comment for end of icons in whichkey

--- init.lua
+++ init.lua
@@ -321,7 +321,7 @@ require('lazy').setup({
           { '<leader>t', group = '[T]oggle' },
           { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
         },
-      },
+      }, --- End of icons
     },
   },
 
