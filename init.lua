0d194fbb14f5b592c060184a48e90ef012e55610
Edison Pizarro Godoy <edison.pizarro@front.com>
2025-04-16 11:39:07 -0400

    add git blame

--- init.lua
+++ init.lua
@@ -259,6 +259,25 @@ require('lazy').setup({
   -- NOTE: WakaTime is a plugin that tracks your time spent in Neovim
   { 'wakatime/vim-wakatime', lazy = false },
 
+  -- NOTE: git blame
+  {
+    'f-person/git-blame.nvim',
+    -- load the plugin at startup
+    event = 'VeryLazy',
+    -- Because of the keys part, you will be lazy loading this plugin.
+    -- The plugin will only load once one of the keys is used.
+    -- If you want to load the plugin at startup, add something like event = "VeryLazy",
+    -- or lazy = false. One of both options will work.
+    opts = {
+      -- your configuration comes here
+      -- for example
+      enabled = true, -- if you want to enable the plugin
+      message_template = ' <summary> • <date> • <author> • <<sha>>', -- template for the blame message, check the Message template section for more options
+      date_format = '%d-%m-%Y %H:%M:%S', -- template for the date, check Date format section for more options
+      virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+    },
+  },
+
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
