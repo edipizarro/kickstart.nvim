0a0745b20262c01f89dc5cb622f66447ca15c396
Edison Pizarro Godoy <edison.pizarro@front.com>
2025-06-10 15:09:17 -0400

    Add git link copy

--- init.lua
+++ init.lua
@@ -311,6 +311,29 @@ require('lazy').setup({
     end,
   },
 
+  -- NOTE: Git link copies github link of line of code
+  {
+    'juacker/git-link.nvim',
+    keys = {
+      {
+        '<leader>gu',
+        function()
+          require('git-link.main').copy_line_url()
+        end,
+        desc = 'Copy code link to clipboard',
+        mode = { 'n', 'x' },
+      },
+      {
+        '<leader>go',
+        function()
+          require('git-link.main').open_line_url()
+        end,
+        desc = 'Open code link in browser',
+        mode = { 'n', 'x' },
+      },
+    },
+  },
+
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
