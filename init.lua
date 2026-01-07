02c7886179f083a67a70fca53ba82cc8ae0edf65
Edison Pizarro Godoy <edison.pizarro@front.com>
2026-01-07 12:38:50 -0300

    Wezterm types

--- init.lua
+++ init.lua
@@ -528,10 +528,12 @@ require('lazy').setup({
       library = {
         -- Load luvit types when the `vim.uv` word is found
         { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+        { path = 'wezterm-types', words = { 'wezterm' } },
       },
     },
   },
   { 'Bilal2453/luvit-meta', lazy = true },
+  { 'justinsgithub/wezterm-types', lazy = true },
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
